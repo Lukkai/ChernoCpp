573d99b27a9c7b168fde62c58de1b6228f3a83a8
Lukkai <luki99942@gmail.com>
2020-04-04 01:16:43 +0200

    ptr is null - if and else statements

--- ConditionsAndBranches/newMain.cpp
+++ ConditionsAndBranches/newMain.cpp
@@ -6,6 +6,10 @@ int main()
 	const char* ptr = nullptr;
 	if (ptr) // (ptr != nullptr) nie trzeba bo kiedy  jest nullptr to i tak nie wejdzie do srodka petli
 		Log(ptr);		//jesli moj wskaznik wskazuje na jakis element i nie jest null to wykunuje instrukcje
+	else if (ptr == "Hello")
+		Log("Ptr says Hello!");
+	else
+		Log("Ptr is null!");
 	
 	std::cin.get();
 }
