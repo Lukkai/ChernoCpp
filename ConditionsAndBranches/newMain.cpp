0837cd28f629d9ff945771d3de36d23f0da6fc0c
Lukkai <luki99942@gmail.com>
2020-04-04 01:32:58 +0200

    nice additional update

--- ConditionsAndBranches/newMain.cpp
+++ ConditionsAndBranches/newMain.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	const char* ptr = "Hello";
+	const char* ptr = "Hello, remember to dring water and get sufficient amount of sleep";
 	if (ptr) // (ptr != nullptr) nie trzeba bo kiedy  jest nullptr to i tak nie wejdzie do srodka petli
 		Log(ptr);		//jesli moj wskaznik wskazuje na jakis element i nie jest null to wykunuje instrukcje
 	/*else if (ptr == "Hello")
