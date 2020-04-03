0037ba5fdeb4b2738f605c1c315b9c0b83b1d511
Lukkai <luki99942@gmail.com>
2020-04-03 22:55:03 +0200

    debugger and loops init

---
+++ Debugger/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Log.h"
+
+int main()
+{
+	int a = 8;
+	a++;
+	const char* string = "Hello";
+
+	for (int i = 0; i < 5; i++)
+	{
+		const char c = string[i];
+		std::cout << c << std::endl;
+	}
+
+	Log("Hello World!");
+	std::cin.get();
+}
