819d23d88a10fa73ed3b32c4a3c06bdee286e117
Lukkai <luki99942@gmail.com>
2020-04-04 01:18:45 +0200

    else if and hello ptr

--- ConditionsAndBranches/newMain.cpp
+++ ConditionsAndBranches/newMain.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-	const char* ptr = nullptr;
+	const char* ptr = "Hello";
 	if (ptr) // (ptr != nullptr) nie trzeba bo kiedy  jest nullptr to i tak nie wejdzie do srodka petli
 		Log(ptr);		//jesli moj wskaznik wskazuje na jakis element i nie jest null to wykunuje instrukcje
-	else if (ptr == "Hello")
-		Log("Ptr says Hello!");
+	/*else if (ptr == "Hello")
+		Log("Ptr says Hello!");		to sie nigdy nie wykona bo wykona sie albo if albo else if, pierwszy if sie wykona, drugi nie*/
 	else
 		Log("Ptr is null!");
 	
