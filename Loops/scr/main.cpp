7effd7c127f2a52b1916ac042eda5fd33efaa7ee
Lukkai <luki99942@gmail.com>
2020-04-04 02:35:06 +0200

    alternative for(; i<5 ; ) and bool statement loop example

--- Loops/scr/main.cpp
+++ Loops/scr/main.cpp
@@ -3,9 +3,14 @@
 
 int main(void)
 {
-	for (i = 0; i < 5; i++)
+	int i = 0;
+	bool condition = true;
+	for (; condition;)
 	{
 		Log("Hello World!");
+		i++;
+		if (!(i < 5))
+			condition = false;	//jesli i jest juz wieksze niz 5 to warunek petli nie jest spelniony
 	}
 	std::cin.get();
 }
