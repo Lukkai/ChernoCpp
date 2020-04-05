a8c6e171f9f788e30268bc836ffdf243641a9369
Lukkai <luki99942@gmail.com>
2020-04-05 19:14:25 +0200

    static in classes and structures in cpp initial commit

---
+++ 10_StaticInClasses/scr/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*tworzac zmienne static w klasie tak naprawde w kazdej instancji wtedy posiadam t¹ sama jedna zmienna w ka¿dej z nich. 
+jesli w jednej klasie zmienie ta zmienna na inna wartosc to wszystkie klasy równie¿ bêd¹ mia³y inna wartosc tej zmiennej*/
+
+
+int main()
+{
+	
+
+	std::cin.get();
+}
