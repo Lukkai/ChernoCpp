e8f94431b1d2cfc5eb393a569996568b65d1e152
Lukkai <luki99942@gmail.com>
2020-04-05 23:54:39 +0200

    local or global scope of a variable example update

--- 11_LocalStatic/scr/Main.cpp
+++ 11_LocalStatic/scr/Main.cpp
@@ -3,14 +3,15 @@
 /*lifetime of a viariable and scope of a varaible*/
 /*a local static is a variable which has a lifetime of our whole program, however its scope is going to be only inside our meant body of a function/if statement/class/whatever we mean it to be in*/
 
-int i = 0;  //for Function() it doesnt matter in this example if its static or not
+//int i = 0;  //for Function() it doesnt matter in this example if its static or not
 
 void Function()
 {
 	//int i = 0;
-	//static int i = 0;	//if i make it static its very similar to int i being declared outside this function. 
-						//outside it wont matter for this function if its static or not for now
-						//but "i" being outside the body of this function is accesible from anywhere so it can follow us to some major issues 
+	static int i = 0;	//if i make it static its very similar to int i being declared outside this function. 
+						//outside it wont matter for this function if its static or not for now. if made an example, look at comment int i = 0 above at line 6
+						//but "i" being outside the body of this function is a global variable and is accesible from anywhere so it can follow us to some major issues
+						//static int i being inside of this function will affect its scope, so it will be accessible only within scope of this function. this way it wont be global
 	i++;
 	std::cout << i << std::endl;
 }
@@ -19,7 +20,7 @@ void Function()
 int main(void)
 {
 	Function();
-	i = 10; //examplary issue with i beibng accessible from anywhere
+	//i = 10; //examplary issue with i beibng accessible from anywhere //with i being static inside the function is only accessible inside this function. its not a global variable
 	Function();
 	Function();
 	Function();
