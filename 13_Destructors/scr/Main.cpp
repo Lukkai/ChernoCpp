ae53637c52742bffb0c6ea0ef7616653735ce25f
Lukkai <luki99942@gmail.com>
2020-04-06 02:15:21 +0200

    13_destructors initial commit

--- 13_Destructors/scr/Main.cpp
+++ 13_Destructors/scr/Main.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 
+/*destructor - evil twin of constructor. it deletes the constructor*/
+#include <iostream>
+
 class Entity
 {
 public:
+	float X, Y;
 
+	Entity()  //we can initialize an object using a constructor with parameters we set or without 'em
+	{
+		X = 0.0f;
+		Y = 0.0f;
+		std::cout << "Created Entity!" << std::endl;
+	}
+
+	Entity(float x, float y)
+	{
+		X = x;
+		Y = y;
+	}
+	~Entity()
+	{
+		std::cout << "Destroyed Entity!" << std::endl;
+	}
+
+	void Print()
+	{
+		std::cout << X << ", " << Y << std::endl;
+	}
 };
 
-	int main(void)
+class Log
+{
+	/*private:
+		Log() {}*/ //if defined a constructor by us in private field we get an error cause i wont be able to access the constructor
+public:
+	//Log() = delete; //if i dont want to have a default constructor made by compiler
+	static void Write()
 	{
-		
-		std::cin.get();
+
 	}
+};
+
+void Function()
+{
+	Entity e;
+	e.Print();
+}
+
+int main(void)
+{
+	Function();
+	std::cin.get();
+}
